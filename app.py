b2b4dca8e0dd157df184a1f806c83df3c1bdc4d4
AnhPhuN <90109952+AnhPhuN@users.noreply.github.com>
2021-12-04 11:41:32 -0500

    f

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def index():
 
         try: 
             element = WebDriverWait(driver, 4).until(
-                    EC.presence_of_element_located((By.XPATH, "//*[text()='activated your kit']")))
+                    EC.presence_of_element_located((By.XPATH, "//*[text()='You've activated your kit! Now, collect a sample.']")))
         except TimeoutException:
             return render_template("message.html", title = "Barcode/Accession Invalid", message = "Your \
             Barcode or Accession Number is incorrect. Please retry the form and check your values are correct.")
