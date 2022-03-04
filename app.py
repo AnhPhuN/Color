c2ad3102c6b83365aa10973dc9ffe30f13df855a
AnhPhuN <90109952+AnhPhuN@users.noreply.github.com>
2022-03-04 01:08:11 -0500

    F

--- app.py
+++ app.py
@@ -143,12 +143,12 @@ def index():
         # when all info has been verified, run automator:   
         driver = webdriver.Chrome()
         
-        # chrome_options = webdriver.ChromeOptions()
-        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-        # chrome_options.add_argument("--no-sandbox")
-        # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
         # get website
         driver.get("https://home.color.com/covid/activation")
